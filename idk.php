ecf2ed70076be43e547bf4431455219af12884e7
Regina-Da-Hot-Mess <eishune1708@gmail.com>
2022-02-10 21:23:20 +0630

    First commit

---
+++ idk.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "I'm so sick of myself";
+
+        $a=12;
+        $b=34;
+       echo"sum=". ($a+$b);
+
+       echo "<p>Duncan's Diner</p>";
+        $y=1000;
+        echo "<p>US dollar = \$$y</p>";
+
+        $i="Hello";
+        $j="My little munchinks";
+        echo $i.",".$j."</br>";
+
+      
+    ?>
+</body>
+</html>
---
+++ roomates.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Oh my god they were room mates</p>
+
+    <?php
+        echo "You little bitch";
+        for ($i=1; $i <5 ; $i++) { 
+            echo "<p>um chile..anyways so $i</p>";
+
+        };
+
+        echo "<br>";
+        $a=12345;
+        $b=9022;
+        $c=569;
+        $sum=($a+$b+$c);
+       
+        echo "Sum=".($a+$b); echo "<br>";
+        echo "Avg=".($sum/3);
+    ?>
+
+    
+</body>
+</html>
